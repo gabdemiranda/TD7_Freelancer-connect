47638970671539013322aff77951cb3e60f08fd1
gabdemiranda <gabriel.demcastro@gmail.com>
2021-10-10 03:10:47 -0300

    project model created and test for homepage project features

---
+++ db/migrate/20211010054223_create_projects.rb
@@ -0,0 +1,14 @@
+class CreateProjects < ActiveRecord::Migration[6.1]
+  def change
+    create_table :projects do |t|
+      t.string :title
+      t.string :description
+      t.string :skills
+      t.decimal :value
+      t.date :end_date
+      t.string :work_style
+
+      t.timestamps
+    end
+  end
+end
--- spec/system/visitor_views_homepage_spec.rb
+++ spec/system/visitor_views_homepage_spec.rb
@@ -9,4 +9,19 @@ describe "Visitor visits homepage" do
   expect(page).to have_content("Contrate o melhor profissional para sua necessidade!")
   
   end
+  it 'and views available projects' do
+    Project.create!({ title: 'Site para cadastro de imóveis',
+                    description: 'Preciso de um site que cadastre imóveis de vários tipos e em várias locações',
+                    skills: 'Ruby on Rails',
+                    value: 50,
+                    end_date: '10/11/2021',
+                    work_style: 'Remota'
+                    })
+
+    visit root_path
+    
+    expect(page).to have_content("Site para cadastro de imóveis")
+    expect(page).to have_content("Valor máximo por hora: 50 reais")
+    expect(page).to have_content("Habilidades desejadas: Ruby on Rails")
+  end
 end
