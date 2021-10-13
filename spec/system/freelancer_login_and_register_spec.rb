d091cea3be305b4929ec1d1c6a58e14945b11323
gabdemiranda <gabriel.demcastro@gmail.com>
2021-10-13 02:47:16 -0300

    Regular and Freelancer users now can register new account

--- spec/system/freelancer_login_and_register_spec.rb
+++ spec/system/freelancer_login_and_register_spec.rb
@@ -6,13 +6,13 @@ describe 'Freelancer logs in' do
 
       visit root_path
       click_on 'Entrar como freelancer'
-      fill_in 'Email', with: freelancer.email
+      fill_in 'E-mail', with: freelancer.email
       fill_in 'Senha', with: freelancer.password
       within 'form' do
         click_on 'Entrar'
       end
 
-      expect(page).to have_content('Login efetuado com sucesso!')
+      expect(page).to have_content('Login efetuado com sucesso.')
       expect(page).to have_content(freelancer.email)
       expect(page).to have_link('Logout')
       expect(page).to have_link('Cadastrar projeto')
@@ -21,13 +21,13 @@ describe 'Freelancer logs in' do
     it 'and must fill all information' do
       visit root_path
       click_on 'Entrar como freelancer'
-      fill_in 'Email', with: ''
+      fill_in 'E-mail', with: ''
       fill_in 'Senha', with: ''
       within 'form' do
         click_on 'Entrar'
       end
 
-      expect(page).to have_content('Email ou senha inválida.')
+      expect(page).to have_content('E-mail ou senha inválidos')
     end
 
     it 'and logs out' do
@@ -37,10 +37,22 @@ describe 'Freelancer logs in' do
       visit root_path
       click_on 'Logout'
 
-      expect(page).to have_content('Saiu com sucesso')
+      expect(page).to have_content('Logout efetuado com sucesso')
       expect(page).to_not have_content(freelancer.email)
       expect(page).to_not have_link('Logout')
       expect(page).to have_link('Entrar como freelancer')
       expect(page).to_not have_link('Cadastrar projeto')
+    end
+
+    it 'registers new freelancer user' do
+      visit root_path
+      click_on 'Entrar como freelancer'
+      click_on 'Registrar-se como freelancer'
+      fill_in 'E-mail', with: 'teste@ufreelancer.com'
+      fill_in 'Senha', with: '123sd456'
+      fill_in 'Confirme sua senha', with: '123sd456'
+      click_button 'Registrar'
+  
+      expect(page).to have_content('Você realizou seu registro com sucesso')
     end
   end
--- spec/system/regular_user_logsin_and_registers_spec.rb
+++ spec/system/regular_user_logsin_and_registers_spec.rb
@@ -1,48 +1,59 @@
 require 'rails_helper'
 
 describe 'as regular user' do
-  it 'successfully' do
+  it 'successfully logs in' do
     regular_user = RegularUser.create!(email: 'janice@teste.com.br', password: '123456789')
 
     visit root_path
     click_on 'Entrar como usuário'
-    fill_in 'Email', with: regular_user.email
+    fill_in 'E-mail', with: regular_user.email
     fill_in 'Senha', with: regular_user.password
     within 'form' do
       click_on 'Entrar'
     end
 
-    expect(page).to have_content('Login efetuado com sucesso!')
+    expect(page).to have_content('Login efetuado com sucesso')
     expect(page).to have_content(regular_user.email)
     expect(page).to have_link('Logout')
     expect(page).not_to have_link('Entrar')
     expect(page).to have_link('Cadastrar projeto')
   end
 
-  it 'and logs out' do
+  it 'logs out' do
     regular_user = RegularUser.create!(email: 'janice@teste.com.br', password: '123456789')
 
     login_as regular_user, scope: :regular_user
     visit root_path
     click_on 'Logout'
 
-    expect(page).to have_content('Saiu com sucesso')
+    expect(page).to have_content('Logout efetuado com sucesso')
     expect(page).to_not have_content(regular_user.email)
     expect(page).to_not have_link('Logout')
     expect(page).to have_link('Entrar')
     expect(page).to_not have_link('Cadastrar projeto')
   end
 
-  it 'and must fill all information' do
+  it 'you must fill all information' do
     visit root_path
     click_on 'Entrar como usuário'
-    fill_in 'Email', with: ''
+    fill_in 'E-mail', with: ''
     fill_in 'Senha', with: ''
     within 'form' do
       click_on 'Entrar'
     end
 
-    expect(page).to have_content('Email ou senha inválida.')
+    expect(page).to have_content('E-mail ou senha inválidos.')
   end
 
+  it 'registers new regular user' do
+    visit root_path
+    click_on 'Entrar como usuário'
+    click_on 'Registrar-se como usuário'
+    fill_in 'E-mail', with: 'teste@usuario.com'
+    fill_in 'Senha', with: '123456'
+    fill_in 'Confirme sua senha', with: '123456'
+    click_button 'Registrar'
+
+    expect(page).to have_content('Você realizou seu registro com sucesso')
+  end
 end
