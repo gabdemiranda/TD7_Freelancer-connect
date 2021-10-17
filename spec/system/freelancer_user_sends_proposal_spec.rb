12f9053513237769b7141e3af5f28d932c205fff
gabdemiranda <gabriel.demcastro@gmail.com>
2021-10-17 17:49:53 -0300

    Freelancer user cannot send proposal if the project is closed

--- spec/system/freelancer_user_sends_proposal_spec.rb
+++ spec/system/freelancer_user_sends_proposal_spec.rb
@@ -61,4 +61,32 @@ describe 'as a Freelancer I can' do
     expect(page).to have_content('Complete seu perfil para fazer propostas')
     expect(page).to_not have_content('Fazer proposta')
   end
+
+  it 'not send a proposal if project is closed' do
+    regularuser = RegularUser.create!(email: 'tom@user.com.br', password: '123456')
+    Project.create!({ title: 'Site para cadastro de imóveis',
+      description: 'Preciso de um site que cadastre imóveis de vários tipos e em várias locações',
+      skills: 'Ruby on Rails',
+      value: 50,
+      end_date: '10/11/2021',
+      work_style: 'Remota',
+      regular_user_id: 1,
+      status: 10
+      })
+    freelancer_user = FreelancerUser.create!(email: 'freelancer@teste.com.br', password: '1234tyy')  
+    profile = Profile.create!(freelancer_user_id: freelancer_user.id, 
+                fullname: 'Gabriela Martins', 
+                socialname: 'Fernando Martins',
+                description: 'Fullstack dev', 
+                education: 'Formado em sistemas de informação',
+                birthdate: '14/06/1996', 
+                experience: 'Três anos trabalhando na Rebase'
+                )
+
+    login_as freelancer_user, scope: :freelancer_user
+    visit root_path
+    click_on 'Site para cadastro de imóveis'
+
+    expect(page).to_not have_content('Fazer proposta')
+  end
 end
